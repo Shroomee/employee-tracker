a67baa08bdc60365cdf718ea40bd7a1d5eba668c
unknown <dudeomous@gmail.com>
2023-04-19 17:36:07 -0400

    added roles table in schema with reference to department_id

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -1,8 +1,15 @@
 --departments table--
-CREATE TABLE departments (
-    department_id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(255)
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
 );
 --roles table--
-
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+)
 --employee table
